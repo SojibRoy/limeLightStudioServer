6a8588a7f1bff5af8b890c65caa52f2ae60a97f0
ROY <you@example.com>
2022-11-08 19:26:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// Middle waears
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ldwl1cr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/',(req,res) => {
+    res.send('photograpgy server is running')
+})
+
+app.listen(port, () => {
+    console.log(`photography server is running on ${port}`)
+})
